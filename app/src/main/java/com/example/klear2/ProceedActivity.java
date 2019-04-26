eac93fa06477ae0aac851c7a926c96186d86e212
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2019-04-27 02:03:24 +0530

    Date picker added

--- app/src/main/java/com/example/klear2/ProceedActivity.java
+++ app/src/main/java/com/example/klear2/ProceedActivity.java
@@ -1,11 +1,18 @@
 package com.example.klear2;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.text.format.DateFormat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,19 +21,68 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ProceedActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.Calendar;
+
+public class ProceedActivity extends FragmentActivity implements OnMapReadyCallback, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     GoogleMap map;
     Button done;
+    Button pickdate;
+    int day, month, year, hours, minute;
+    int dayFinal, monthFinal, yearFinal, hoursFinal, minuteFinal;
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        yearFinal = year;
+        monthFinal = month + 1;
+        dayFinal = dayOfMonth;
+
+        Calendar c = Calendar.getInstance();
+        hours = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(ProceedActivity.this,ProceedActivity.this,
+                hours, minute, DateFormat.is24HourFormat(this));
+        timePickerDialog.show();
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        hoursFinal = hourOfDay;
+        minuteFinal = minute;
+
+        tv_datetime.setText("Schedule Pickup for " + hoursFinal + ":" + minuteFinal + ", " + dayFinal + "/" + monthFinal + "/" + yearFinal + ".");
+
+    }
+
+    TextView tv_datetime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proceed);
 
+        pickdate = (Button)findViewById(R.id.pickdate);
+        tv_datetime = (TextView)findViewById(R.id.tv_datetime);
+
+        pickdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(ProceedActivity.this,ProceedActivity.this,
+                        year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        done = (Button)findViewById(R.id.done);
+        done = (Button)findViewById(R.id.btDone);
         done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
